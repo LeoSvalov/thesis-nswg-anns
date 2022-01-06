3ca6a0033b3d75ef566481f1dac27815188ae2e8
LeoSvalov <leos3112@gmail.com>
2022-01-06 18:53:55 +0500

    minor works: documentation, delete unnecessary and restructuring fixes #2 #3 #4

--- product_quantization.py
+++ product_quantization.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.cluster import KMeans
 import pickle
 import itertools
+import multiprocessing
+import faiss
 
 def nptype_from_bits(c):
     if c <= 8:
@@ -22,10 +24,8 @@ class PQ:
         self.random_state = 420
         self.partition_size = -1
 
-
-        #### TODO: SAVE DICTS TO PICKLE ####
         self.subvector_centroids = {}
-        self.kmeans = {} #??
+        self.kmeans = {}
         self.lookup = {}
 
     @staticmethod
@@ -33,7 +33,7 @@ class PQ:
         return np.sum(np.square(vec2 - vec1), axis=-1)
 
     def save_index(self):
-        pickle.dump([self.subvector_centroids,self.kmeans,self.lookup], open( "index.pickle", "wb"))
+        pickle.dump([self.subvector_centroids, self.kmeans, self.lookup], open("index.pickle", "wb"))
 
     def _get_data_partition(self, train_data, partition_idx):
         partition_start = partition_idx * self.partition_size
@@ -41,20 +41,25 @@ class PQ:
         train_data_partition = train_data[:, partition_start:partition_end]
         return train_data_partition
 
-    def _compress_partition(self, partition_idx: int, train_data_partition):
-        km = KMeans(n_clusters=self.n_centroids, n_init=1, random_state=self.random_state)
-        compressed_data_partition = km.fit_predict(train_data_partition).astype(self.int_type)
-        partition_centroids = km.cluster_centers_
-        return partition_idx, compressed_data_partition, partition_centroids, km
+    def _compress_partition(self, train_data_partition):
+        # km = KMeans(n_clusters=self.n_centroids, n_init=1, random_state=self.random_state)
+        # compressed_data_partition = km.fit_predict(train_data_partition).astype(self.int_type)
+        # partition_centroids = km.cluster_centers_
+
+        km = faiss.Kmeans(d=len(train_data_partition[0]), k=self.n_centroids, nredo=1, seed=self.random_state)
+        km.train(np.ascontiguousarray(train_data_partition).astype(np.float32))
+        compressed_data_partition = km.index.search(np.ascontiguousarray(train_data_partition).astype(np.float32), 1)[1].astype(self.int_type)
+        partition_centroids = km.centroids
+        return compressed_data_partition, partition_centroids, km
 
     def calculate_lookup(self, centroids):
         table = np.zeros(shape=(self.n_centroids, self.n_centroids))
-        for v in itertools.combinations(enumerate(centroids),2):
+        for v in itertools.combinations(enumerate(centroids), 2):
             i = v[0][0]
             j = v[1][0]
             c1 = v[0][1]
             c2 = v[1][1]
-            table[i][j] = self.calc_dist(c1,c2)
+            table[i][j] = self.calc_dist(c1, c2)
             table[j][i] = table[i][j]
         return table
 
@@ -66,43 +71,79 @@ class PQ:
         d = len(train_data[0])
         self.partition_size = d // self.n_subvectors
 
+
+        # with multiprocessing.Pool() as pool:
+        #     params = [(partition_idx, self._get_data_partition(train_data, partition_idx)) for partition_idx in range(self.n_subvectors)]
+
+        #     for partition_idx, compressed_data_partition, partition_centroids, km in pool.starmap(self._compress_partition, params):
         for partition_idx in range(self.n_subvectors):
             train_data_partition = self._get_data_partition(train_data, partition_idx)
-            partition_idx, compressed_data_partition, partition_centroids, km = \
-                self._compress_partition(partition_idx, train_data_partition)
-            self.compressed_data[:, partition_idx] = compressed_data_partition
+            compressed_data_partition, partition_centroids, km = self._compress_partition(train_data_partition)
+            # print(compressed_data_partition, partition_centroids)
+            # print("-"*10)
+            self.compressed_data[:, partition_idx] = compressed_data_partition.reshape(1, -1)[0]
             self.subvector_centroids[partition_idx] = partition_centroids
             self.kmeans[partition_idx] = km
             self.lookup[partition_idx] = self.calculate_lookup(partition_centroids)
 
-        if to_save: self.save_index()
+        if to_save:
+            self.save_index()
 
+    def quantize_query(self, partition_idx, test_sample):
+        partition_start = partition_idx * self.partition_size
+        partition_end = (partition_idx + 1) * self.partition_size
+        test_sample_partition = test_sample[partition_start:partition_end]
+        centroids_partition = self.compressed_data[:, partition_idx]
+        compressed_test_partition = self.kmeans[partition_idx].predict(test_sample_partition.reshape(1, -1))
+        return partition_idx, centroids_partition, compressed_test_partition
+
+    def unquntize_query(self, partition_idx, test_sample):
+        partition_start = partition_idx * self.partition_size
+        partition_end = (partition_idx + 1) * self.partition_size
+        test_sample_partition = test_sample[partition_start:partition_end]
+        centroids_partition = self.subvector_centroids[partition_idx]
+        return partition_idx, centroids_partition, test_sample_partition
 
     def predict_single(self, test_sample: np.ndarray, nearest_neighbors: int, using_lookup=False):
 
         if using_lookup:
             nb_stored_samples = len(self.compressed_data)
             distances = np.empty(shape=(nb_stored_samples, self.n_subvectors), dtype=np.float64)
+            # with multiprocessing.Pool() as pool:
+            #     params = [(partition_idx, test_sample) for partition_idx in range(self.n_subvectors)]
+            #     for partition_idx, centroids_partition, compressed_test_partition in pool.starmap(self.quantize_query, params):
+            #         distances[:, partition_idx] = [self.lookup[partition_idx][compressed_test_partition][0][i] for i in
+            #                                        centroids_partition]
+            # pool.close()
+
+
             for partition_idx in range(self.n_subvectors):
                 partition_start = partition_idx * self.partition_size
                 partition_end = (partition_idx + 1) * self.partition_size
                 test_sample_partition = test_sample[partition_start:partition_end]
                 centroids_partition = self.compressed_data[:, partition_idx]
-                compressed_test_partition = self.kmeans[partition_idx].predict(test_sample_partition.reshape(1, -1))
-                distances[:, partition_idx] = [self.lookup[partition_idx][compressed_test_partition][0][i] for i in
+                # compressed_test_partition = self.kmeans[partition_idx].predict(test_sample_partition.reshape(1, -1))
+                compressed_test_partition = self.kmeans[partition_idx].index.search(np.ascontiguousarray(test_sample_partition.reshape(1, -1)).astype(np.float32), 1)[1][0][0]
+                # print(self.lookup[partition_idx][compressed_test_partition][0])
+                # print("---"*10)
+                distances[:, partition_idx] = [self.lookup[partition_idx][compressed_test_partition][i] for i in
                                                centroids_partition]
 
-            # print(distances)
             distances = list(map(lambda x: sum(x), distances))
             indices = np.argpartition(distances, nearest_neighbors)
         else:
             distances = np.empty(shape=(self.n_centroids, self.n_subvectors), dtype=np.float64)
+            # with multiprocessing.Pool() as pool:
+            #     params = [(partition_idx, test_sample) for partition_idx in range(self.n_subvectors)]
+            #     for partition_idx, centroids_partition, test_sample_partition in pool.starmap(self.unquntize_query, params):
+            #         distances[:, partition_idx] = self.calc_dist(test_sample_partition, centroids_partition)
 
             for partition_idx in range(self.n_subvectors):
                 partition_start = partition_idx * self.partition_size
                 partition_end = (partition_idx + 1) * self.partition_size
                 test_sample_partition = test_sample[partition_start:partition_end]
                 centroids_partition = self.subvector_centroids[partition_idx]
+
                 # todo replace lookup
                 distances[:, partition_idx] = self.calc_dist(test_sample_partition, centroids_partition)
 
@@ -116,7 +157,6 @@ class PQ:
 
         return indices[:nearest_neighbors]
 
-
     def predict(self, test_data: np.ndarray, nearest_neighbors: int, using_lookup=False):
-        preds = [self.predict_single(row, nearest_neighbors, using_lookup) for row in test_data]
+        preds = [self.predict_single(test_sample, nearest_neighbors, using_lookup) for test_sample in test_data]
         return np.array(preds)
